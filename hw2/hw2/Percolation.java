208bdb099e968aacd7f226f6b3cc297d8a002721
Yicheng Xia <icheng.hsia@gmail.com>
2021-03-17 20:05:38 +0800

    completed hw2_v5 (fixed constant time requirement without backwash)

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -6,10 +6,12 @@ public class Percolation {
 
     private int N;
     private int openNum;
-    private int sentinel;
+    private int sentinelT;
+    private int sentinelB;
     private boolean percolated;
     private boolean[] openIndex;
     private WeightedQuickUnionUF grid;
+    private WeightedQuickUnionUF gridB;
 
     // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) {
@@ -22,8 +24,12 @@ public class Percolation {
         for (int i = 0; i < N * N; i += 1) {
             openIndex[i] = false;
         }
+        // only virtual top site
         grid = new WeightedQuickUnionUF(N * N + 1);
-        sentinel = N * N;
+        sentinelT = N * N;
+        // virtual top and bottom sites
+        gridB = new WeightedQuickUnionUF(N * N + 2);
+        sentinelB = N * N + 1;
         percolated = false;
     }
 
@@ -35,7 +41,7 @@ public class Percolation {
         return row * N + col;
     }
 
-    // open the site (row, col) if it is not open already                
+    // open the site (row, col) if it is not open already             
     public void open(int row, int col) {
         if (!inRange(row, col)) {
             throw new IndexOutOfBoundsException("Invalid arg: row " + row + ", col " + col + ".");
@@ -43,25 +49,31 @@ public class Percolation {
         if (!openIndex[getIndex(row, col)]) {
             openIndex[getIndex(row, col)] = true;
             if (row == 0) {
-                grid.union(getIndex(0, col), sentinel);
+                grid.union(getIndex(row, col), sentinelT);
+                gridB.union(getIndex(row, col), sentinelT);
+            }
+            if (row == N - 1) {
+                gridB.union(getIndex(row, col), sentinelB);
             }
             openNum += 1;
             if (inRange(row - 1, col) && isOpen(row - 1, col)) {
                 grid.union(getIndex(row, col), getIndex(row - 1, col));
+                gridB.union(getIndex(row, col), getIndex(row - 1, col));
             }
             if (inRange(row + 1, col) && isOpen(row + 1, col)) {
                 grid.union(getIndex(row, col), getIndex(row + 1, col));
+                gridB.union(getIndex(row, col), getIndex(row + 1, col));
             }
             if (inRange(row, col - 1) && isOpen(row, col - 1)) {
                 grid.union(getIndex(row, col), getIndex(row, col - 1));
+                gridB.union(getIndex(row, col), getIndex(row, col - 1));
             }
             if (inRange(row, col + 1) && isOpen(row, col + 1)) {
                 grid.union(getIndex(row, col), getIndex(row, col + 1));
+                gridB.union(getIndex(row, col), getIndex(row, col + 1));
             }
-            for (int c = 0; c < N; c += 1) {
-                if (grid.connected(getIndex(N - 1, c), sentinel)) {
+            if (gridB.connected(sentinelT, sentinelB)) {
                     percolated = true;
-                }
             }
         }
     }
@@ -79,7 +91,7 @@ public class Percolation {
         if (!inRange(row, col)) {
             throw new IndexOutOfBoundsException("Invalid arg: row " + row + ", col " + col + ".");
         }
-        return grid.connected(getIndex(row, col), sentinel);
+        return grid.connected(getIndex(row, col), sentinelT);
     }
 
     // number of open sites
