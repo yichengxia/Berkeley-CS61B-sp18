08de23d459a2a73e2677726307fe34e2f85819ad
Yicheng Xia <icheng.hsia@gmail.com>
2021-03-23 22:57:03 +0800

    completed lab11_v1.1 (optional MazeCycles.java to be finished)

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,9 @@
 package lab11.graphs;
 
+import java.util.Random;
+
 /**
- *  @author Josh Hug
+ *  @author Yicheng Xia
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -9,6 +11,8 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    
+    private int[] parent;
 
     public MazeCycles(Maze m) {
         super(m);
@@ -16,9 +20,30 @@ public class MazeCycles extends MazeExplorer {
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        parent = new int[maze.V()];
+        Random rand = new Random();
+        int startX = rand.nextInt(maze.N());
+        int startY = rand.nextInt(maze.N());
+        int s = maze.xyTo1D(startX, startY);
+        marked[s] = true;
+        distTo[s] = 0;
+        parent[s] = s;
+        dfs(s);
     }
 
-    // Helper methods go here
+    private void dfs(int v) {
+        for (int u : maze.adj(v)) {
+            if (!marked[u]) {
+                marked[u] = true;
+                distTo[u] = distTo[v] + 1;
+                announce();
+                parent[u] = v;
+                dfs(u);
+            } else if (u != parent[v]) {
+                // need to search for the common parent node, reverse and link the loop
+                // to be finished
+                return;
+            }
+        }
+    }
 }
-
